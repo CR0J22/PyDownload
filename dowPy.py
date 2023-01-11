23e6135589519eb4a1f1131da4a3fadb45c24ac1
Cr0j--22 <ronaldoj50919@gmail.com>
2023-01-11 16:23:42 -0300

    Melhora da performace e adição da função para baixar audio

--- dowPy.py
+++ dowPy.py
@@ -1,13 +1,15 @@
-import pytube 
+from pytube import YouTube,Playlist
+from pytube.cli import on_progress
 
 """ ("Cr0j") """
 
-tipo = input("Baixar:\n1-Playlsit\n2-Video\n3-Exit\n")
+tipo = input("Baixar:\n1-Playlsit\n2-Video\n3-Audio\n4-Exit\n")
 
+"""Playlist"""
 if tipo == "1":
     link = input("Insira a url da playlsit:")
 
-    yt = pytube.Playlist(link)
+    yt = Playlist(link)
 
     for video in yt.videos:
         print(f"O download de {video} começou")
@@ -15,14 +17,37 @@ if tipo == "1":
         print(f"O download de {video} acabou")
 
     print(f"O download de {link} acabou")
+
+"""Video"""
 if tipo == "2":
     link = input("Insira a url do video:")
 
     """ 22 """
-    yt = pytube.Playlist(link)
+    yt = YouTube(link)
 
     yt.streams.get_highest_resolution().download()
 
     print(f"O download de {link} acabou")
+
+""" Audio """
+if tipo == "3":
+    link = input("Insira a url do video => audio:")
+
+    """ 22 """
+    yt = YouTube(link)
+
+    audio = yt.streams.filter(only_audio=True)[0]
+    audio.download()
+
+    print(f"O download de {link} acabou")
+    
+""" Teste de Barra de Progresso """  
+if tipo =="4":
+    link=input('enter url:')
+    yt=YouTube(link,on_progress_callback=on_progress)
+    videos=yt.streams.first()
+    videos.download()
+    print("(:")
+    
 else:
     exit
